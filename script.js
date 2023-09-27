7f9ff42cebf4c5eb7dfceaf70b66ba489ccdd2a1
sykul <luke.flatley@outlook.com>
2023-09-27 18:17:13 +0200

    add event listeners to equals

--- script.js
+++ script.js
@@ -35,16 +35,30 @@ function operate(x, operator, y) {
     }
 }
 
-function numberOnScreen(number) {
+function numberOnScreen(number, prevNumber) {
+    if 
     const screenP = document.querySelector("div.screen > p");
-    screenP.textContent = number;
+    screenP.textContent += number;
 }
 
 const numberButtons = document.querySelectorAll(".number");
 numberButtons.forEach(function(numberButton) {
     numberButton.addEventListener("click", function () {
-        const number = numberButton.textContent;
-        numberOnScreen(number);
+        const screenP = document.querySelector("div.screen > p");
+        const prevNumber = Number(screenP.textContent);
+        const number = Number(numberButton.textContent);
+        numberOnScreen(number, prevNumber);
     })
+})
+
+const operators = document.querySelectorAll(".operator");
+operators.forEach(function() {
+    
+})
+
+const equals = document.querySelector("equals");
+equals.addEventListener("click", function() {
+    const screenP = document.querySelector("div.screen > p");
+    operate();
 })
 
